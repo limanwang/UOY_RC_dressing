11bb31268ae5b4b58718cb69e9d86aecb1c09f86
Liman Wang <ssee02131@gmail.com>
2024-10-22 21:41:29 +0100

    Update joystick_bathing.py

--- template/joystick_bathing.py
+++ template/joystick_bathing.py
@@ -47,67 +47,67 @@ def _main(use_graphics=False):
             elif button_data[10]:  # Select button
                 mode = 0  # IK mode
             
-            if mode:
-                print('controlling move base')
-                # Control forward and backward movement
-                if button_data[4]:  # Button Y moves forward
-                    robot.EnabledNativeIK(False)
-                    robot.MoveForward(0.5, 0.2)  # Move forward 0.5m at 0.2 speed
-                    env.step()
-                elif button_data[0]:  # Button A moves backward
-                    robot.EnabledNativeIK(False)
-                    robot.MoveBack(0.5, 0.2)  # Move backward 0.5m at 0.2 speed
-                    env.step()
-
-                # Control left and right turns
-                if button_data[3]:  # Button X turns left
-                    robot.EnabledNativeIK(False)
-                    robot.TurnLeft(90, 0.2)  # Turn left 90 degrees at 0.2 speed
-                    env.step()
-                elif button_data[1]:  # Button B turns right
-                    robot.EnabledNativeIK(False)
-                    robot.TurnRight(90, 0.2)  # Turn right 90 degrees at 0.2 speed
-                    env.step()
-
-                if button_data[9]:  # Button R2 (brake)
-                    robot.MoveForward(0.5, 0)  # Move forward 0.5m at 0 speed
-                    env.step()
-
-            else:
-                print('controlling arm')
-                # Use IK to precisely control position
-                move_z = axis_data[1] * 0.1  # Left joystick controls forward/backward
-                move_y = axis_data[3] * 0.1  # Right joystick controls up/down
-                y += move_y
-
-                if y <= -10:
-                    y = -10
-                elif y >= 0:
-                    y = 0
-
-                quotient, remainder = divmod(y, -2)
-                if quotient == 1:
-                    total_y = [-2, remainder, 0, 0]
-                elif quotient == 2:
-                    total_y = [-2, -2, remainder, 0]
-                elif quotient == 3:
-                    total_y = [-2, -2, -2, remainder]
-                elif quotient == 0:
-                    total_y = [remainder, 0, 0, 0]
-
-                z += move_z
-                if z >= 2:
-                    z = 2
-                elif z <= -10:
-                    z = -10
-                
-                print('z is:', z)
-                print('y is:', y)
-                print('y is:', total_y)
-
+            # if mode:
+                # print('controlling move base')
+            # Control forward and backward movement
+            if button_data[4]:  # Button Y moves forward
                 robot.EnabledNativeIK(False)
-                robot.SetJointVelocity([0, 0, z, total_y[0], total_y[1], total_y[2], total_y[3], 0, 0, 0, 0, 0, 0, 0])
+                robot.MoveForward(0.5, 0.2)  # Move forward 0.5m at 0.2 speed
                 env.step()
+            elif button_data[0]:  # Button A moves backward
+                robot.EnabledNativeIK(False)
+                robot.MoveBack(0.5, 0.2)  # Move backward 0.5m at 0.2 speed
+                env.step()
+
+            # Control left and right turns
+            if button_data[3]:  # Button X turns left
+                robot.EnabledNativeIK(False)
+                robot.TurnLeft(90, 0.2)  # Turn left 90 degrees at 0.2 speed
+                env.step()
+            elif button_data[1]:  # Button B turns right
+                robot.EnabledNativeIK(False)
+                robot.TurnRight(90, 0.2)  # Turn right 90 degrees at 0.2 speed
+                env.step()
+
+            if button_data[9]:  # Button R2 (brake)
+                robot.MoveForward(0.5, 0)  # Move forward 0.5m at 0 speed
+                env.step()
+
+            # else:
+            #     print('controlling arm')
+            # Use IK to precisely control position
+            move_z = axis_data[1] * 0.1  # Left joystick controls forward/backward
+            move_y = axis_data[3] * 0.1  # Right joystick controls up/down
+            y += move_y
+
+            if y <= -10:
+                y = -10
+            elif y >= 0:
+                y = 0
+
+            quotient, remainder = divmod(y, -2)
+            if quotient == 1:
+                total_y = [-2, remainder, 0, 0]
+            elif quotient == 2:
+                total_y = [-2, -2, remainder, 0]
+            elif quotient == 3:
+                total_y = [-2, -2, -2, remainder]
+            elif quotient == 0:
+                total_y = [remainder, 0, 0, 0]
+
+            z += move_z
+            if z >= 2:
+                z = 2
+            elif z <= -10:
+                z = -10
+            
+            print('z is:', z)
+            print('y is:', y)
+            print('y is:', total_y)
+
+            robot.EnabledNativeIK(False)
+            robot.SetJointVelocity([0, 0, z, total_y[0], total_y[1], total_y[2], total_y[3], 0, 0, 0, 0, 0, 0, 0])
+            env.step()
 
             # Control gripper open and close
             if button_data[6]:  # Button L1 opens the gripper
